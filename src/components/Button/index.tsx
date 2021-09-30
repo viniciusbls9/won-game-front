c6da04e16bc76ff89c0aaa0d20754ffc1773ea33
Vinicius <beneditoloura@gmail.com>
2021-09-30 09:11:39 -0300

    ✨ feat: create withIcon modifier to use icon in the button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,14 +5,17 @@ export type ButtonProps = {
   children?: ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  icon?: ReactNode
 }
 
 const Button = ({
   children,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  icon
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth}>
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+    {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
