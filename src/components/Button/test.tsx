a940161db23ef67389cf31f77d5130db6cc0ec47
Vinicius <beneditoloura@gmail.com>
2021-09-30 09:20:49 -0300

    ✅ test: create snapshot to default button and ensure render icon

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,16 +1,19 @@
 import { renderWithTheme } from 'utils/tests/helpers'
 import { screen } from '@testing-library/react'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 import Button from '.'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the small size by default', () => {
@@ -35,5 +38,13 @@ describe('<Button />', () => {
       padding: '0.8rem 4.8rem',
       'font-size': '1.6rem'
     })
+  })
+
+  it('should render an icon version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
