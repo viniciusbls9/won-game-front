8ae05f347ecaa2f28705bcf5026b6e1d0d32287b
Vinicius <beneditoloura@gmail.com>
2021-09-30 08:57:07 -0300

    ✨ feat: ensure Button component with fullWidth prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,10 +4,17 @@ import * as S from './styles'
 export type ButtonProps = {
   children?: ReactNode
   size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
 }
 
-const Button = ({ children, size = 'medium' }: ButtonProps) => (
-  <S.Wrapper size={size}>{!!children && <span>{children}</span>}</S.Wrapper>
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth = false
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth}>
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
 )
 
 export default Button
