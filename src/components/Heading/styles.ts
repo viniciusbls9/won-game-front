bea70bdc335b052806d902c496a7e4fdd92aced9
Vinicius <beneditoloura@gmail.com>
2021-10-07 18:11:40 -0300

    ✨ feat: ensure render a heading with a small size

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -3,6 +3,22 @@ import media from 'styled-media-query'
 import { HeadingProps } from '.'
 
 const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+
+    &::after {
+      width: 3rem;
+    }
+  `,
+
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+  `,
+
   lineLeft: (theme: DefaultTheme) => css`
     padding-left: ${theme.spacings.xxsmall};
     border-left: 0.7rem solid ${theme.colors.secondary};
@@ -23,9 +39,9 @@ const wrapperModifiers = {
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom }) => css`
-    font-size: ${theme.font.sizes.xlarge};
+  ${({ theme, color, lineLeft, lineBottom, size }) => css`
     color: ${theme.colors[color!]};
+    font-size: ${theme.font.sizes.xlarge};
 
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.xxlarge};
@@ -33,5 +49,6 @@ export const Wrapper = styled.h2<HeadingProps>`
 
     ${lineLeft && wrapperModifiers.lineLeft(theme)}
     ${lineBottom && wrapperModifiers.lineBottom(theme)}
+    ${!!size && wrapperModifiers[size](theme)}
   `}
 `
