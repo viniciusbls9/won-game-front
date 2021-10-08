cf6dcb45c557c4b493116ab01eb5702c05d44604
Vinicius <beneditoloura@gmail.com>
2021-10-08 17:35:10 -0300

    ✅ test: create first test to banner component

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,28 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+import { screen } from '@testing-library/react'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render banner corretly with props', () => {
+    renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+  })
+})
