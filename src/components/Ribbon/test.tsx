feab0d18188fc7811ea1592bcd3a3a5c41eaf4c4
Vinicius <beneditoloura@gmail.com>
2021-10-11 16:56:10 -0300

    ✅ test: ensure render with de secondary color if the prop is passed

--- src/components/Ribbon/test.tsx
+++ src/components/Ribbon/test.tsx
@@ -17,4 +17,12 @@ describe('<Ribbon />', () => {
       backgroundColor: '#F231A5'
     })
   })
+
+  it('should render with the secondary color', () => {
+    renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
+
+    expect(screen.getByText(/Best Seller/i)).toHaveStyle({
+      backgroundColor: '#3cd3c1'
+    })
+  })
 })
