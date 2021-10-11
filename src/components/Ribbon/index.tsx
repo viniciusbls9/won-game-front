85413c112a9d0732d972d16ad570270548ebaf40
Vinicius <beneditoloura@gmail.com>
2021-10-11 14:48:44 -0300

    ✨ feat: create a Ribbon component

---
+++ src/components/Ribbon/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react'
+import * as S from './styles'
+
+export type RibbonProps = {
+  children: ReactNode
+}
+
+const Ribbon = ({ children }: RibbonProps) => <S.Wrapper>{children}</S.Wrapper>
+
+export default Ribbon
