64efbfb8dd200a3ecb4c4d4f74e98377cd5bcccd
Vinicius <beneditoloura@gmail.com>
2021-10-14 08:11:12 -0300

    ✅ test: ensure render ribbon with default values

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -27,4 +27,21 @@ describe('<Banner />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render a ribbon with default values', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="normal"
+        ribbonColor="primary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#F231A5' })
+    expect(ribbon).toHaveStyle({ height: '3.6rem', fontSize: '1.4rem' })
+  })
 })
