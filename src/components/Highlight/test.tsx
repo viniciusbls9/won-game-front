28c782275122d085b4d8ae3cacee34f517d95d8a
Vinicius <beneditoloura@gmail.com>
2021-10-14 08:47:44 -0300

    ✅ test: ensure render simple headings and buttons on Highlight component

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -1,15 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { screen } from '@testing-library/react'
 
 import Highlight from '.'
 
+const props = {
+  title: 'Heading 1',
+  subTitle: 'Heading 2',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2'
+}
+
 describe('<Highlight />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Highlight />)
+  it('should render headings and buttons', () => {
+    renderWithTheme(<Highlight {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: /Highlight/i })
+      screen.getByRole('heading', { name: /Heading 1/i })
     ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /Heading 2/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /Buy now/i })).toBeInTheDocument()
   })
 })
