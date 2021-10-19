4fb9b63ad570c95bf3ce2c7db03761601d5f40ee
Vinicius <beneditoloura@gmail.com>
2021-10-19 08:22:12 -0300

    🚨 fix: resolve lint problems

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -2,9 +2,8 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import GameCard from '.'
 
 export default {
-title: 'GameCard',
-component: GameCard
+  title: 'GameCard',
+  component: GameCard
 } as Meta
 
-export const Default: Story = () =>
-<GameCard />
+export const Default: Story = () => <GameCard />
--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -2,14 +2,14 @@ import { render, screen } from '@testing-library/react'
 
 import GameCard from '.'
 
-describe('
-<GameCard />', () => {
-it('should render the heading', () => {
-const { container } = render(
-<GameCard />)
+describe('<GameCard />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<GameCard />)
 
-expect(screen.getByRole('heading', { name: /GameCard/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /GameCard/i })
+    ).toBeInTheDocument()
 
-expect(container.firstChild).toMatchSnapshot()
-})
+    expect(container.firstChild).toMatchSnapshot()
+  })
 })
