ced31c8db5bfb7ce61a53579742a74b0272c2ac6
Vinicius <beneditoloura@gmail.com>
2021-10-19 07:56:19 -0300

    ✅ test: ensure render a float image in Highlight component

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -41,4 +41,13 @@ describe('<Highlight />', () => {
       backgroundImage: `url(${props.backgroundImage})`
     })
   })
+
+  it('should render float image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    )
+  })
 })
