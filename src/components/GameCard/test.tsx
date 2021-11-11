6cf52581355ed3e699ddb84e16647d7a582305cb
Vinicius <beneditoloura@gmail.com>
2021-11-11 07:54:41 -0300

    ✅ test: should render correctly component

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,15 +1,29 @@
-import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { screen } from '@testing-library/react'
 
 import GameCard from '.'
 
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://i.picsum.photos/id/1010/1600/400.jpg?hmac=aHo3G1_n46RGzMM7jzyqMKUbSCfPPkHlm52FEvbShaw',
+  price: 'R$ 235,00'
+}
+
 describe('<GameCard />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCard />)
-
-    expect(
-      screen.getByRole('heading', { name: /GameCard/i })
-    ).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render correctly', () => {
+    // renderizar o gamecard
+    renderWithTheme(<GameCard {...props} />)
+    // verifiquem se o title foi renderizado
+    expect(screen.getByRole('heading', { name: props.title }))
+    // verifiquem se a developer foi renderizado
+    expect(screen.getByRole('heading', { name: props.developer }))
+    // verifiquem se a img foi renderizado
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+    // verifiquem se o price foi renderizado
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
   })
 })
