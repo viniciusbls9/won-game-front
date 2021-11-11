b64dd4bc97826932953184a9697660e7178facb3
Vinicius <beneditoloura@gmail.com>
2021-11-11 07:53:56 -0300

    ✨ feat: render gameCard component

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,9 +1,19 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameCard from '.'
+import GameCard, { GameCardProps } from '.'
 
 export default {
   title: 'GameCard',
-  component: GameCard
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://i.picsum.photos/id/1010/1600/400.jpg?hmac=aHo3G1_n46RGzMM7jzyqMKUbSCfPPkHlm52FEvbShaw',
+    price: 'R$ 235,00'
+  }
 } as Meta
 
-export const Default: Story = () => <GameCard />
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
