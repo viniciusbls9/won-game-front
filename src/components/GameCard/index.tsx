9afa9825029037c771fe4128ae4ec328685d4856
Vinicius <beneditoloura@gmail.com>
2021-11-18 07:00:41 -0300

    ✨ feat: create gameCard Ribbon

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -3,6 +3,7 @@ import {
   AddShoppingCart,
   Favorite
 } from '@styled-icons/material-outlined'
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
 import Button from '../Button'
 import * as S from './styles'
 
@@ -13,6 +14,9 @@ export type GameCardProps = {
   price: string
   promotionalPrice?: string
   favorite?: boolean
+  ribbon?: React.ReactNode
+  ribbonColor?: RibbonColors
+  ribbonSize?: RibbonSizes
   onFav?: () => void
 }
 
@@ -23,9 +27,17 @@ const GameCard = ({
   price,
   promotionalPrice,
   favorite = false,
+  ribbon,
+  ribbonColor = 'primary',
+  ribbonSize = 'small',
   onFav
 }: GameCardProps) => (
   <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon color={ribbonColor} size={ribbonSize}>
+        {ribbon}
+      </Ribbon>
+    )}
     <S.ImageBox>
       <img src={img} alt={title} />
     </S.ImageBox>
--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -9,10 +9,11 @@ export default {
     developer: 'Rockstar Games',
     img: 'https://i.picsum.photos/id/1010/1600/400.jpg?hmac=aHo3G1_n46RGzMM7jzyqMKUbSCfPPkHlm52FEvbShaw',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 200,00'
+    promotionalPrice: 'R$ 215,00'
   },
   argTypes: {
-    onFav: { action: 'clicked' }
+    onFav: { action: 'clicked' },
+    ribbon: { type: 'string' }
   },
   parameters: {
     backgrounds: {
@@ -26,3 +27,15 @@ export const Default: Story<GameCardProps> = (args) => (
     <GameCard {...args} />
   </div>
 )
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'small',
+  ribbonColor: 'primary'
+}
