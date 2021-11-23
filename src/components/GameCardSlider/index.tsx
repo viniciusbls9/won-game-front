9ef7002f558e36f0df28c0f9b052f7db711ebea2
Vinicius <beneditoloura@gmail.com>
2021-11-23 08:04:36 -0300

    ✨ feat: create GameCardSlider and update styles

---
+++ src/components/GameCardSlider/index.tsx
@@ -0,0 +1,61 @@
+import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
+import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
+import Slider, { SliderSettings } from 'components/Slider'
+import GameCard, { GameCardProps } from '../GameCard'
+import * as S from './styles'
+
+export type GameCardSliderProps = {
+  items: GameCardProps[]
+  color?: 'white' | 'black'
+}
+
+const settings: SliderSettings = {
+  arrows: true,
+  prevArrow: <ArrowLeft aria-label="previous games" />,
+  nextArrow: <ArrowRight aria-label="next games" />,
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.2
+      }
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        arrows: false,
+        slidesToShow: 1.1
+      }
+    }
+  ]
+}
+
+const GameCardSlider = ({ items, color = 'black' }: GameCardSliderProps) => (
+  <S.Wrapper color={color}>
+    <Slider settings={settings}>
+      {items.map((item, index) => {
+        return <GameCard key={index} {...item} />
+      })}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default GameCardSlider
