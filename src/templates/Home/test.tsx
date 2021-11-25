7e0aadb563b152dc342b55d833f8c4a3510f3dd7
Vinicius <beneditoloura@gmail.com>
2021-11-25 07:52:00 -0300

    ✅ test: ensure render correctly components

---
+++ src/templates/Home/test.tsx
@@ -0,0 +1,36 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+import { screen } from '@testing-library/react'
+
+import Home from '.'
+
+describe('<Home />', () => {
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /contact/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the sections', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Most Popular/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Upcomming/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Free Games/i })
+    ).toBeInTheDocument()
+  })
+})
