fac9f5134bfc584324cb0475e493880d7c6610e4
Vinicius <beneditoloura@gmail.com>
2021-11-30 07:14:18 -0300

    ♻️ refactor: create mocks files

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,16 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighlightProps } from '.'
+import item from './mocks'
 
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Read Dead is back!',
-    subtitle: 'Come see John’s new adventures',
-    buttonLabel: 'Buy now',
-    buttonLink: '/games/rdr2',
-    backgroundImage: '/img/red-dead-img.jpg'
-  }
+  args: { ...item }
 } as Meta
 
 export const Default: Story<HighlightProps> = (args) => (
