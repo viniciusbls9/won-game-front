a8a6d2e861355497694ec9cabf0da665bc0081a5
Vinicius <beneditoloura@gmail.com>
2021-12-14 07:55:02 -0300

    ✨feat: create checkbox styles

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -97,7 +97,7 @@ export const MenuFull = styled.nav<MenuFullProps>`
     right: 0;
     height: 100vh;
     overflow: hidden;
-    transition: opacity 0.3s ease-in-out;
+    transition: opacity 0.3s ${theme.transition.default};
     opacity: ${isOpen ? 1 : 0};
     pointer-events: ${isOpen ? 'all' : 'none'};
 
@@ -125,12 +125,12 @@ export const MenuFull = styled.nav<MenuFullProps>`
       font-size: ${theme.font.sizes.xlarge};
       margin-bottom: ${theme.spacings.small};
       transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
-      transition: transform 0.3s ease-in-out;
+      transition: transform 0.3s ${theme.transition.default};
     }
 
     ${RegisterBox} {
       transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
-      transition: transform 0.3s ease-in-out;
+      transition: transform 0.3s ${theme.transition.default};
     }
   `};
 `
