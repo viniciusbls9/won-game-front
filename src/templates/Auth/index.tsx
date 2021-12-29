b38c3db3fd2d1463bbdb3f93699b374de30f1107
Vinicius <beneditoloura@gmail.com>
2021-12-29 11:54:39 -0300

    ✨ feat: create Auth template structure and props

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,8 +1,33 @@
+import { ReactNode } from 'react'
+import { Heading, Logo } from 'components'
 import * as S from './styles'
 
-const Auth = () => (
+type AuthProps = {
+  title: string
+  children: ReactNode
+}
+
+const Auth = ({ children, title }: AuthProps) => (
   <S.Wrapper>
-    <h1>Auth</h1>
+    <S.BannerBlock>
+      <Logo />
+
+      <Heading>All yours favorite games in one place</Heading>
+      <S.Subtitle>
+        <strong>WON</strong> is the best and most complete gaming plataform.
+      </S.Subtitle>
+
+      <S.Footer>Won games 2020 © Todos os Direitos Reservados</S.Footer>
+    </S.BannerBlock>
+
+    <S.Content>
+      <Logo color="black" size="large" />
+      <Heading color="black" lineColor="secondary" lineLeft>
+        {title}
+      </Heading>
+
+      {children}
+    </S.Content>
   </S.Wrapper>
 )
 
