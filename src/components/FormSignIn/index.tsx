cbaee1dd70b56de6068b06c84102477b30570125
Vinicius <beneditoloura@gmail.com>
2022-01-06 06:57:48 -0300

    ✨feat: finished FormSignIn component

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,8 +1,39 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Email, Lock } from 'styled-icons/material-outlined'
+import Link from 'next/link'
+
+import { Button } from 'components'
+import TextField from 'components/TextField'
 import * as S from './styles'
 
 const FormSignIn = () => (
   <S.Wrapper>
-    <h1>FormSignIn</h1>
+    <form>
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+      <S.ForgotPassword>Forgot your password?</S.ForgotPassword>
+
+      <Button size="large" fullWidth>
+        Sign in now
+      </Button>
+
+      <S.FormLink>
+        Don't have an account?{' '}
+        <Link href="/sign-up">
+          <a>Sign up</a>
+        </Link>
+      </S.FormLink>
+    </form>
   </S.Wrapper>
 )
 
