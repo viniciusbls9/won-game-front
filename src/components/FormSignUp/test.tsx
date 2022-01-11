1f2c6b75ae24c2c79d29aad1dce34c46c8a3b23c
Vinicius <beneditoloura@gmail.com>
2022-01-11 07:18:27 -0300

    ✅ test: ensure render the correctly form

---
+++ src/components/FormSignUp/test.tsx
@@ -0,0 +1,20 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignUp from '.'
+
+describe('<FormSignUp />', () => {
+  it('should render the form', () => {
+    const { container } = renderWithTheme(<FormSignUp />)
+
+    expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Confirm password')).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign up now/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
