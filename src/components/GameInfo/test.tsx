a713a5002f1ce0c1c0c7492e5bf8d7e8241a2cf1
Vinicius <beneditoloura@gmail.com>
2022-01-13 07:41:51 -0300

    ✅ test: ensure render a buttons

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -15,20 +15,24 @@ describe('<GameInfo />', () => {
   it('should render game informations', () => {
     renderWithTheme(<GameInfo {...props} />)
 
-    // esperar por um heading (title)
     expect(
       screen.getByRole('heading', { name: props.title })
     ).toBeInTheDocument()
-    // esperar por description
+
     expect(screen.getByText(/\$210,00/i)).toBeInTheDocument()
-    // esperar pelo price
+
     expect(screen.getByText(/game description/i)).toBeInTheDocument()
   })
 
-  // it('should render buttons', () => {
-  //   renderWithTheme(<GameInfo {...props} />)
+  it('should render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
 
-  //   // esperar button add to cart
-  //   // esperar button wishlist
-  // })
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
+  })
 })
