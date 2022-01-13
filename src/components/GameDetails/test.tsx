977f8d167963673242b5427e2965d49124621ae6
Vinicius <beneditoloura@gmail.com>
2022-01-13 20:02:13 -0300

    ✅ test: ensure render the blocks on GameDetails component

---
+++ src/components/GameDetails/test.tsx
@@ -0,0 +1,38 @@
+import '../../../.jest/match-media-mock'
+import { screen } from '@testing-library/react'
+
+import { GameDetails } from 'components'
+import { renderWithTheme } from 'utils/tests/helpers'
+import { GameDetailsProps } from '.'
+
+const props: GameDetailsProps = {
+  developer: 'Different Tales',
+  plataforms: ['windows', 'mac', 'linux'],
+  releaseDate: '2020-11-21T23:00:00'
+}
+
+describe('<GameDetails />', () => {
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Developer/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /release date/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /plataforms/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Developer/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
+  })
+})
