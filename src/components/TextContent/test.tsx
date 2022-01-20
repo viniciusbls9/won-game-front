c7e2dc7df5b46cee3c84a24ec0a33f89ff2fcee7
Vinicius <beneditoloura@gmail.com>
2022-01-20 06:11:11 -0300

    ✅ test: ensure render different color on mobile and desktop

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -29,4 +29,24 @@ describe('<TextContent />', () => {
       screen.queryByRole('heading', { name: /description/i })
     ).not.toBeInTheDocument()
   })
+
+  it('should render different color on mobile and desktop', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    const wrapper = screen.getByRole('heading', {
+      name: /description/i
+    }).parentElement
+
+    expect(wrapper).toHaveStyle({
+      color: '#FAFAFA'
+    })
+
+    expect(wrapper).toHaveStyleRule('color', '#030517', {
+      media: '(min-width: 768px)'
+    })
+
+    expect(
+      screen.getByRole('heading', { name: /description/i })
+    ).toBeInTheDocument()
+  })
 })
