e85be6d2dae90553c9863c12713b896e77913dda
Vinicius <beneditoloura@gmail.com>
2022-01-20 06:08:34 -0300

    ✅ test: ensure render the title and content TextContent

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -21,32 +21,4 @@ describe('<TextContent />', () => {
       screen.getByRole('heading', { name: /content/i })
     ).toBeInTheDocument()
   })
-
-  it('should render without title', () => {
-    renderWithTheme(<TextContent content={props.content} />)
-
-    expect(
-      screen.queryByRole('heading', { name: /description/i })
-    ).not.toBeInTheDocument()
-  })
-
-  it('should render the title and content', () => {
-    renderWithTheme(<TextContent {...props} />)
-
-    const wrapper = screen.getByRole('heading', {
-      name: /description/i
-    }).parentElement
-
-    expect(wrapper).toHaveStyle({
-      color: '#FAFAFA'
-    })
-
-    expect(wrapper).toHaveStyleRule('color', '#030517', {
-      media: '(min-width: 768px)'
-    })
-
-    expect(
-      screen.getByRole('heading', { name: /description/i })
-    ).toBeInTheDocument()
-  })
 })
