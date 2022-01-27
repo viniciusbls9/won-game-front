0a7dfca92ad16307d61c184e25153d38758e2c84
Vinicius <beneditoloura@gmail.com>
2022-01-27 08:05:22 -0300

    ✅ test: ensure render gallery and handle opan modal

---
+++ src/components/Gallery/test.tsx
@@ -0,0 +1,38 @@
+import '../../../.jest/match-media-mock'
+import { fireEvent, screen } from '@testing-library/react'
+
+import { Gallery } from 'components'
+import galleryMock from 'mocks/galleryMock'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+describe('<Gallery />', () => {
+  it('should render thumbnails as buttons', () => {
+    renderWithTheme(<Gallery items={galleryMock.slice(0, 2)} />)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    ).toHaveAttribute('src', galleryMock[0].src)
+
+    expect(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    ).toHaveAttribute('src', galleryMock[1].src)
+  })
+
+  it('should handle open modal', () => {
+    renderWithTheme(<Gallery items={galleryMock.slice(0, 2)} />)
+
+    // selecionar o nosso modal
+    const modal = screen.getByLabelText('modal')
+
+    // verificar se o modal ta escondido
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+
+    // clicar no botão de abrir o modal e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+  })
+})
