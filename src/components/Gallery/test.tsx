dc3ce5284decf9aab5eb7ef74a1f416b400daf5f
Vinicius <beneditoloura@gmail.com>
2022-02-01 08:05:42 -0300

    ✅ test: ensure render a close modal when esc key pressed

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -54,4 +54,26 @@ describe('<Gallery />', () => {
     expect(modal.getAttribute('aria-hidden')).toBe('true')
     expect(modal).toHaveStyle({ opacity: 0 })
   })
+
+  it('should handle close modal when key esc is pressed', () => {
+    const { container } = renderWithTheme(
+      <Gallery items={galleryMock.slice(0, 2)} />
+    )
+
+    // selecionar o nosso modal
+    const modal = screen.getByLabelText('modal')
+
+    // clicar no botão de abrir o modal e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+
+    // clicar para fechar
+    fireEvent.keyUp(container, { key: 'Escape' })
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+  })
 })
