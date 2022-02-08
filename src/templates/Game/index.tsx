2ba6b81efa16f465fa318d8255b4d4b2e52601f2
Vinicius <beneditoloura@gmail.com>
2022-02-08 07:46:56 -0300

    ✨ feat: create Game page template

---
+++ src/templates/Game/index.tsx
@@ -0,0 +1,9 @@
+import { Base } from 'templates'
+
+const Game = () => (
+  <Base>
+    <h1>Game</h1>
+  </Base>
+)
+
+export default Game
--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,3 +1,4 @@
 export { default as Auth } from './Auth'
 export { default as Base } from './Base'
 export { default as Home } from './Home'
+export { default as Game } from './Game'
