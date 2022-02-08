a4e7e8b89a3b9e266cbe16e64f05f56afef96e2b
Vinicius <beneditoloura@gmail.com>
2022-02-08 08:27:14 -0300

    ✅ test: ensure render a cover image

--- src/templates/Game/test.tsx
+++ src/templates/Game/test.tsx
@@ -4,10 +4,14 @@ import { screen } from '@testing-library/react'
 import { Game } from 'templates'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+const props = {
+  cover:
+    'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg'
+}
+
 describe('<Game />', () => {
   it('should render the heading', () => {
-    const { container } = renderWithTheme(<Game />)
-    expect(screen.getByRole('heading', { name: /Game/i })).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
+    renderWithTheme(<Game {...props} />)
+    expect(screen.getByLabelText('cover')).toBeInTheDocument()
   })
 })
