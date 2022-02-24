8615e2710ded5bce6b2af0e5a1a52c67bb5747ed
Vinicius <beneditoloura@gmail.com>
2022-02-24 08:08:49 -0300

    ✨ feat: finished Game template page

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,13 +1,33 @@
+import { Showcase, TextContent } from 'components'
+import { HighlightProps } from 'components/Highlight'
+import { GameCardProps } from 'components/GameCard'
+import Gallery, { GalleryImageProps } from 'components/Gallery'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
+import GameDetails, { GameDetailsProps } from 'components/GameDetails'
 import { Base } from 'templates'
 import * as S from './styles'
 
 export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
+  gallery?: GalleryImageProps[]
+  description: string
+  details: GameDetailsProps
+  upcomingGames: GameCardProps[]
+  upcomingHighlight: HighlightProps
+  recommendedGames: GameCardProps[]
 }
 
-const Game = ({ cover, gameInfo }: GameTemplateProps) => (
+const Game = ({
+  cover,
+  gameInfo,
+  gallery,
+  description,
+  details,
+  upcomingGames,
+  upcomingHighlight,
+  recommendedGames
+}: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
 
@@ -15,6 +35,26 @@ const Game = ({ cover, gameInfo }: GameTemplateProps) => (
       <S.SectionGameInfo>
         <GameInfo {...gameInfo} />
       </S.SectionGameInfo>
+
+      <S.SectionGallery>
+        {!!gallery && <Gallery items={gallery} />}
+      </S.SectionGallery>
+
+      <S.SectionDescription>
+        <TextContent title="description" content={description} />
+      </S.SectionDescription>
+
+      <S.SectionGameDetails>
+        <GameDetails {...details} />
+      </S.SectionGameDetails>
+
+      <Showcase
+        title="Upcoming"
+        games={upcomingGames}
+        highlight={upcomingHighlight}
+      />
+
+      <Showcase title="You may like these games" games={recommendedGames} />
     </S.Main>
   </Base>
 )
