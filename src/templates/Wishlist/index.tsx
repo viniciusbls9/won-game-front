dfc9376909c1ab3b4d452e7eeb37ebcb57ba57c8
Vinicius <beneditoloura@gmail.com>
2022-03-08 08:21:24 -0300

    ✨ feat: build wishlist template to receive props

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,13 +1,28 @@
-import { Container, Heading } from 'components'
+import { Container, Heading, Showcase } from 'components'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
 import { Base } from 'templates'
 
-const Wishlist = () => (
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
   </Base>
 )
 
