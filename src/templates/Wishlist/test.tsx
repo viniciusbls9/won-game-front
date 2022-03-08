b277497cb4c486d3a004f82acefc822668cf1399
Vinicius <beneditoloura@gmail.com>
2022-03-08 08:22:16 -0300

    ✅ test: ensure render correctly wishlist template

--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -4,12 +4,28 @@ import { screen } from '@testing-library/react'
 import { Wishlist } from 'templates'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import gamesMock from 'mocks/gameCardSliderMock'
+import highlightMock from 'mocks/highlightMock'
+
+const props = {
+  recommendedGames: gamesMock,
+  recommendedHighlight: highlightMock
+}
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />
+  }
+}))
+
 describe('<Wishlist />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Wishlist />)
+  it('should render correctly', () => {
+    renderWithTheme(<Wishlist {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /Wishlist/i })
     ).toBeInTheDocument()
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
   })
 })
