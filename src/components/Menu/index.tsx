88cf2880cee9e08e009c97bdd0a30954fa341cc0
Vinicius <beneditoloura@gmail.com>
2022-03-08 07:08:52 -0300

    ✨ feat: include menu links

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -27,12 +27,18 @@ const Menu = ({ username }: MenuProps) => {
 
       {/**  bloco logo */}
       <S.LogoWrapper>
-        <Logo hideOnMobile />
+        <Link href="/" passHref>
+          <a>
+            <Logo hideOnMobile />
+          </a>
+        </Link>
       </S.LogoWrapper>
 
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explorer</S.MenuLink>
         </S.MenuNav>
       </MediaMatch>
@@ -60,7 +66,9 @@ const Menu = ({ username }: MenuProps) => {
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explorer</S.MenuLink>
 
           {!!username && (
