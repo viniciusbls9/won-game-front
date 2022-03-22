b037806a83d13ad4cdbb71ca08fb68f99f1c695d
Vinicius <beneditoloura@gmail.com>
2022-03-22 07:35:45 -0300

    ✨ feat: create GameItem Component

---
+++ src/components/GameItem/stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { GameItem } from 'components'
+import { GameItemProps } from 'components/GameItem'
+
+export default {
+  title: 'GameItem',
+  component: GameItem,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00'
+  }
+} as Meta
+
+export const Default: Story<GameItemProps> = (args) => <GameItem {...args} />
