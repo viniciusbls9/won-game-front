59754df13e0da0372aee1c15b8ca63e5d753ccd3
Vinicius <beneditoloura@gmail.com>
2022-03-24 07:49:43 -0300

    ✨ feat: create and finished CartList component

---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { CartList } from 'components'
+import { CartListProps } from '.'
+
+import gameCartListMock from 'mocks/gameCartMock'
+
+export default {
+  title: 'CartList',
+  component: CartList,
+  args: {
+    items: gameCartListMock,
+    total: 'R$ 330,00'
+  },
+  argTypes: {
+    items: {
+      type: ''
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<CartListProps> = (args) => (
+  <div style={{ maxWidth: 800 }}>
+    <CartList {...args} />
+  </div>
+)
