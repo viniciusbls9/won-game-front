52a7a682a38d2afc4660df1eb562934477e4ed9a
Vinicius <beneditoloura@gmail.com>
2022-04-05 08:18:42 -0300

    ✨ feat: create cart page template

---
+++ src/templates/Cart/index.tsx
@@ -0,0 +1,65 @@
+import {
+  Container,
+  Divider,
+  Heading,
+  Showcase,
+  CartList,
+  PaymentOptions,
+  Empty
+} from 'components'
+import { CartListProps } from 'components/CartList'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+import { PaymentOptionsProps } from 'components/PaymentOptions'
+import { Base } from 'templates'
+import * as S from './styles'
+
+export type CartProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+} & CartListProps &
+  Pick<PaymentOptionsProps, 'cards'>
+
+const Cart = ({
+  recommendedGames,
+  recommendedHighlight,
+  items,
+  total,
+  cards
+}: CartProps) => {
+  const handlePayment = () => ({})
+
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My cart
+        </Heading>
+
+        {items.length ? (
+          <S.Content>
+            <CartList items={items} total={total} />
+
+            <PaymentOptions cards={cards} handlePayment={handlePayment} />
+          </S.Content>
+        ) : (
+          <Empty
+            title="Your cart is empty"
+            description="Go back to the store and explore great games and offers"
+            hasLink
+          />
+        )}
+
+        <Divider />
+      </Container>
+
+      <Showcase
+        title="You may like these games"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
+
+export default Cart
