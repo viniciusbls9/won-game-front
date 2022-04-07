eb9285d46a8c5decbcb2b00512a5fc904d44fe97
Vinicius <beneditoloura@gmail.com>
2022-04-07 09:26:54 -0300

    ✅ test: ensure change bgColor to activeLink

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react'
 
 import { ProfileMenu } from 'components'
 import { renderWithTheme } from 'utils/tests/helpers'
+import theme from 'styles/theme'
 
 describe('<ProfileMenu />', () => {
   it('should render the menu', () => {
@@ -15,5 +16,14 @@ describe('<ProfileMenu />', () => {
     expect(screen.getByRole('link', { name: /My cards/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /My orders/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+
+  it('should render the menu with an active link defined', () => {
+    renderWithTheme(<ProfileMenu activeLink="/profile/cards" />)
+
+    expect(screen.getByRole('link', { name: /My cards/i })).toHaveStyle({
+      background: theme.colors.primary,
+      color: theme.colors.white
+    })
   })
 })
