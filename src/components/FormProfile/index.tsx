00073d5daf862c90bd7d5a3e88d42fb16ef69af9
vinicius <beneditoloura@gmail.com>
2022-04-14 17:23:26 -0300

    ✨ feat: create FormProfile component

---
+++ src/components/FormProfile/index.tsx
@@ -0,0 +1,12 @@
+import { Heading } from 'components'
+import * as S from './styles'
+
+const FormProfile = () => (
+  <S.Wrapper>
+    <Heading lineBottom color="black" size="small">
+      My profile
+    </Heading>
+  </S.Wrapper>
+)
+
+export default FormProfile
