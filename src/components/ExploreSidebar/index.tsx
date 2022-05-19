6ea967cea38e4a702434fb88abc24ab1c4e88514
vinicius <beneditoloura@gmail.com>
2022-05-19 07:49:40 -0300

    ✨ feat: include ExploreSideBar checkbox

--- src/components/ExploreSidebar/index.tsx
+++ src/components/ExploreSidebar/index.tsx
@@ -1,4 +1,4 @@
-import { Heading } from 'components'
+import { Heading, Checkbox } from 'components'
 import * as S from './styles'
 
 const ExploreSidebar = () => (
@@ -6,6 +6,13 @@ const ExploreSidebar = () => (
     <Heading lineBottom lineColor="secondary" size="small">
       Price
     </Heading>
+
+    <Checkbox name="under-50" label="Under $50" labelFor="under-50" />
+    <Checkbox name="under-100" label="Under $100" labelFor="under-100" />
+    <Checkbox name="under-150" label="Under $150" labelFor="under-150" />
+    <Checkbox name="under-200" label="Under $200" labelFor="under-200" />
+    <Checkbox name="free" label="Free" labelFor="free" />
+    <Checkbox name="discounted" label="Discounted" labelFor="discounted" />
 
     <Heading lineBottom lineColor="secondary" size="small">
       Sort by
