9cfa222a7b1ec2dd8b977335a3521412e17aa060
vinicius <beneditoloura@gmail.com>
2022-05-31 08:21:59 -0300

    ✨ feat: create Dropdown component

---
+++ src/components/Dropdown/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { Dropdown } from 'components'
+import { DropdownProps } from '.'
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<DropdownProps> = (args) => <Dropdown {...args} />
+
+Default.args = {
+  title: 'Click here',
+  children: 'content'
+}
