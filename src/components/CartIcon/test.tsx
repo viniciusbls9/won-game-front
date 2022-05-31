cbeadc15e25d0a0259dad240e46861424386aa1d
vinicius <beneditoloura@gmail.com>
2022-05-31 08:04:39 -0300

    ✅ test: ensure render component without badge

---
+++ src/components/CartIcon/test.tsx
@@ -0,0 +1,14 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+import { CartIcon } from 'components'
+
+describe('<CartIcon />', () => {
+  it('should render without badge', () => {
+    renderWithTheme(<CartIcon />)
+
+    expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
+    expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
+  })
+})
