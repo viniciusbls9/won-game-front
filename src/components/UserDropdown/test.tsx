ffc9f82a3e01d287d10742d0fff9ce839626118d
vinicius <beneditoloura@gmail.com>
2022-06-03 07:48:43 -0300

    ✅ test: ensure render the menu

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,5 +1,6 @@
 import 'match-media-mock'
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { UserDropdown } from 'components'
 import { renderWithTheme } from 'utils/tests/helpers'
@@ -9,5 +10,18 @@ describe('<UserDropdown />', () => {
     renderWithTheme(<UserDropdown username="John Doe" />)
 
     expect(screen.getByText(/john doe/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="John Doe" />)
+
+    userEvent.click(screen.getByText(/john doe/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
   })
 })
