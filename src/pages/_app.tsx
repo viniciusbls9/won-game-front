f31a8b6476e955c93f5b1a050dab81b293d36c30
Vinicius <beneditoloura@gmail.com>
2023-01-03 10:08:48 -0300

    feat: use ApolloProvider on app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,30 +2,41 @@ import theme from 'styles/theme'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Head>
-        <title>Won Games</title>
-        <link
-          rel="shortcut icon"
-          href="/img/icon-512.png"
-          type="image/x-icon"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/img/icon-512.png"
-          type="image/x-icon"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="The best Game Stores in the word " />
-      </Head>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Head>
+          <title>Won Games</title>
+          <link
+            rel="shortcut icon"
+            href="/img/icon-512.png"
+            type="image/x-icon"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/img/icon-512.png"
+            type="image/x-icon"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="The best Game Stores in the word "
+          />
+        </Head>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
