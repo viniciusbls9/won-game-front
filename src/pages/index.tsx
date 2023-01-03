11ff30a68063fc51d53b18918b6bbde7d1e03325
Vinicius <beneditoloura@gmail.com>
2023-01-03 10:09:30 -0300

    feat: create first query using useQuery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannerMock from 'mocks/bannerSliderMock'
@@ -6,20 +6,17 @@ import highlightMock from 'mocks/highlightMock'
 import gamesMock from 'mocks/gameCardSliderMock'
 
 export default function Index(props: HomeTemplateProps) {
-  const client = new ApolloClient({
-    uri: 'http://localhost:1337/graphql',
-    cache: new InMemoryCache()
-  })
-
-  client.query({
-    query: gql`
-      query getGames {
-        games {
-          name
-        }
+  const { data, loading } = useQuery(gql`
+    query getGames {
+      games {
+        name
       }
-    `
-  })
+    }
+  `)
+
+  if (loading) return <p>Loading...</p>
+
+  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
 
   return <Home {...props} />
 }
