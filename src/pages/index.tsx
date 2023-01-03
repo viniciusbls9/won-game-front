e968477b6a1b28d35a753d8c0c5726f61df2f933
Vinicius <beneditoloura@gmail.com>
2023-01-03 09:55:49 -0300

    feat: create first and simple apollo client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannerMock from 'mocks/bannerSliderMock'
 import highlightMock from 'mocks/highlightMock'
 import gamesMock from 'mocks/gameCardSliderMock'
 
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `
+  })
+
   return <Home {...props} />
 }
 
