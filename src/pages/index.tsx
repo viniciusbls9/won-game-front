86cd894b20f2b284e10fd0a3006ce0fa8f30d32f
Vinicius <beneditoloura@gmail.com>
2023-01-10 09:12:12 -0300

    feat: using async getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,13 @@
-import { gql, useQuery } from '@apollo/client'
-
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannerMock from 'mocks/bannerSliderMock'
 import highlightMock from 'mocks/highlightMock'
 import gamesMock from 'mocks/gameCardSliderMock'
 
 export default function Index(props: HomeTemplateProps) {
-  const { data, loading } = useQuery(gql`
-    query getGames {
-      games {
-        name
-      }
-    }
-  `)
-
-  if (loading) return <p>Loading...</p>
-
-  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
-
   return <Home {...props} />
 }
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
   return {
     props: {
       banners: bannerMock,
