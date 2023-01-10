5dcbe7bf52d8029f2fdce0d7909aa329ceee8723
Vinicius <beneditoloura@gmail.com>
2023-01-10 09:09:22 -0300

    feat: using initializeApollo on SSR to call games

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,46 @@
 import { Games } from 'templates'
 import { GamesTemplateProps } from 'templates/Games'
 import filterItems from 'mocks/exploreSideBarMock'
-import gamesMock from 'mocks/gameCardSliderMock'
+import { initializeApollo } from 'utils/apollo'
+import { gql } from '@apollo/client'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <Games {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryGame {
+        games {
+          name
+          slug
+          cover {
+            url
+          }
+          developers {
+            name
+          }
+          price
+        }
+      }
+    `
+  })
+
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })),
       filterItems
     }
   }
