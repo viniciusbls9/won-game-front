ddb6ae85bb99c48e0259c25c8678e727fd97f1a4
Vinicius <beneditoloura@gmail.com>
2023-01-10 08:04:52 -0300

    feat: create apollo config to work SSR

---
+++ src/utils/apollo.ts
@@ -0,0 +1,38 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client'
+import { useMemo } from 'react'
+
+let apolloClient: ApolloClient<NormalizedCacheObject>
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    cache: new InMemoryCache()
+  })
+}
+
+export function initializeApollo(initialState = {}) {
+  // serve para verificar se já existe uma instancia, para n criar outra
+  const apolloClientGlobal = apolloClient ?? createApolloClient()
+
+  // recuperando os dados de cache
+  if (initialState) {
+    apolloClientGlobal.cache.restore(initialState)
+  }
+
+  // sempre inicializando em SSR com cache limpo
+  if (typeof window === 'undefined') return apolloClientGlobal
+  apolloClient = apolloClient ?? apolloClientGlobal
+
+  return apolloClient
+}
+
+export function useApollo(initialState = {}) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState])
+  return store
+}
