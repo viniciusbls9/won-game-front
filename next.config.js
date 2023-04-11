e7de5b63b6aa40a6309534cc1c2c82ce6894e57f
Vinicius <beneditoloura@gmail.com>
2023-04-11 08:07:40 -0300

    refactor: change nextconfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
     disable: !isProd
   },
   reactStrictMode: true
